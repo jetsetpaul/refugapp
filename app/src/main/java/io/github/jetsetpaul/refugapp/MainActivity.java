22a3ebb00000dcf931aba26742aff5a6b4e7813c
Paul Joiner <pauluntakjoiner@gmail.com>
2017-07-05 19:31:55 -0500

    initial commit

---
+++ app/src/main/java/io/github/jetsetpaul/refugapp/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.jetsetpaul.refugapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
